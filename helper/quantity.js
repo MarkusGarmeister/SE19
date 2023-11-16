bddcae9bd709ba26b11f444cfd56e7c8bd8d29a8
Markus <markusgarmeister@gmail.com>
2023-11-16 18:11:03 +0100

    Fixed the filter didn't worked properly because of id of label

--- helper/quantity.js
+++ helper/quantity.js
@@ -1,3 +1,5 @@
+
+//Outputs the quantity of an assets and is rounded to the 4th decimal, 0.0001 BTC = 3$
 function assetQuantity (amount, price) {
     const quantity = (amount / price).toFixed(4)
     return quantity
